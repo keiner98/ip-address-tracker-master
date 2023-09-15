c2e4908ed18ec75a8d6b9684614cb578d9dd8098
Keiner Molina <keiner1027@gmail.com>
2023-09-15 11:50:40 -0500

    changes in ip-view

--- src/app/UI/components/components.module.ts
+++ src/app/UI/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardsComponent } from './card/card.component';
 import { LoadingComponent } from './loading/loading.component';
+import { ContentCardComponent } from './content-card/content-card.component';
 
 @NgModule({
-  declarations: [CardsComponent, LoadingComponent],
-  exports: [CardsComponent, LoadingComponent],
+  declarations: [CardsComponent, LoadingComponent, ContentCardComponent],
+  exports: [CardsComponent, LoadingComponent, ContentCardComponent],
   imports: [CommonModule],
 })
 export class ComponentsModule {}
---
+++ src/app/UI/components/content-card/content-card.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.scss'],
+})
+export class ContentCardComponent {
+  @Input() title: string = '';
+  @Input() content: string = '';
+}
